01da1adbc34e0f35143d00cc3661a2479941abb3
a.frosali@studenti.gobettivolta.edu.it <a.frosali@studenti.gobettivolta.edu.it>
2021-01-15 08:07:52 +0000

    Inserimento file main.c nel repository

---
+++ main.c
@@ -0,0 +1,12 @@
+/* 	Progetto: Ripasso programmazione in linguaggio C
+Nome file: main.c
+    	Autore: Giancarlo Martini
+*/
+#include <stdio.h>
+
+int main()
+{
+	printf("Ciao\n\n\n");
+	return 0;
+}
+
